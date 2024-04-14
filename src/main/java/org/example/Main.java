ff6484fdf00ad3fc35a5fbb5050b035b4ef6d212
EvgeniaZlobina <ZEA_6590@mail.ru>
2024-04-14 21:10:53 +0500

    the output of the result in the Roman numeral system

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -17,7 +17,7 @@ public class Main {
             if ( length > 3 ) {
                 try {
                     throw new IOException();
-                } catch (IOException e) {
+                } catch (IOException e) { e.printStackTrace();
                     System.out.println( "Output: " );
                     System.out.println( "формат математической операции не удовлетворяет заданию" );
                 }
@@ -35,7 +35,7 @@ public class Main {
                 rimmsks[8] = "IX";
                 rimmsks[9] = "X";
                 String[] arabs = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-                Calculation result = new Calculation(); // вызов метода подсчета
+
                 String part0 = "arab";
                 for (int i = 0; i <= 9; i++) {
                     if ( example[0].equals( rimmsks[i] ) ) {
@@ -53,7 +53,7 @@ public class Main {
                 if ( !part0.equals( part2 ) || !part2.equals( part0 ) ) {
                     try {
                         throw new IOException();
-                    } catch (IOException e) {
+                    } catch (IOException e) { e.printStackTrace();
                         System.out.printf( "Output: " );
                         System.out.println( "используются одновременно разные системы счисления" );
                     }
@@ -62,45 +62,52 @@ public class Main {
 
                     int num1 = 11;
                     int num2 = 12;
+                    int s = 0;
+
                     for (int i = 0; i <= 9; i++) {
                         if ( example[0].equals( rimmsks[i] ) ) {
                             num1 = i + 1;
+                            s = 0;
                             // System.out.println( "РИМСКАЯ1:  " + num1 );
                         }
                     }
                     for (int i = 0; i <= 9; i++) {
                         if ( example[2].equals( rimmsks[i] ) ) {
                             num2 = i + 1;
+                            s = 0;
                             // System.out.println( "РИМСКАЯ2:  " + num2 );
                         }
                     }
-                    if ( num1 < num2 && num1 < 11 && num2 < 12 ) {
+                    if ( num1 < num2 && num1 < 11 && num2 < 12 && example[1].equals("-")) {
                         try {
                             throw new IOException();
-                        } catch (IOException e) {
+                        } catch (IOException e) {e.printStackTrace();
                             System.out.printf( "Output: " );
                             System.out.println( "в римской системе нет отрицательных чисел" );
                         }
                     } else {
                         if ( num1 == 11 ) {
                             num1 = Integer.parseInt( example[0] );
+                            s = 1;
                         }
                         if ( num2 == 12 ) {
                             num2 = Integer.parseInt( example[2] );
+                            s = 1;
                         }
                         if ( num1 < 1 || num2 > 10 || num1 > 10 || num2 < 1 ) {
                             try {
                                 throw new IOException();
-                            } catch (IOException e) {
+                            } catch (IOException e) { e.printStackTrace();
                                 System.out.printf( "Output: " );
                                 System.out.println( "Калькулятор умеет работать только с числами от 1 до 10" );
                             }
 
 
                         } else {
+                            Calculation result = new Calculation(); // вызов метода подсчета
                             result.number1 = num1;// первое число
                             result.number2 = num2; //второе число
-
+                            result.s1 = s;
                             switch ( example[1] ) {
                                 case "+":
                                     result.summa();
@@ -129,29 +136,172 @@ public class Main {
 class Calculation {
     int number1;
     int number2;
-
+    int s1;
 
     void summa() {
         int resultatSumma = number1 + number2; // вычисление суммы
         System.out.println( "Output" );
-        System.out.println( +resultatSumma );
+        if ( s1 == 1 ) {
+            System.out.println( +resultatSumma );
+        } else {
+            switch ( resultatSumma ) {
+                case 1:
+                    System.out.println( "I" );
+                    break;
+                case 2:
+                    System.out.println( "II" );
+                    break;
+                case 3:
+                    System.out.println( "III" );
+                    break;
+                case 4:
+                    System.out.println( "IV" );
+                    break;
+                case 5:
+                    System.out.println( "V" );
+                    break;
+                case 6:
+                    System.out.println( "VI" );
+                    break;
+                case 7:
+                    System.out.println( "VII" );
+                    break;
+                case 8:
+                    System.out.println( "VIII" );
+                    break;
+                case 9:
+                    System.out.println( "IX" );
+                    break;
+                case 10:
+                    System.out.println( "X" );
+                    break;
+            }
+        }
     }
 
     void difference() {
         int resultatDifference = number1 - number2; // вычисление разности
         System.out.println( "Output" );
-        System.out.println( +resultatDifference );
+        if ( s1 == 1 ) {
+            System.out.println( +resultatDifference );
+        } else {
+            switch ( resultatDifference ) {
+                case 1:
+                    System.out.println( "I" );
+                    break;
+                case 2:
+                    System.out.println( "II" );
+                    break;
+                case 3:
+                    System.out.println( "III" );
+                    break;
+                case 4:
+                    System.out.println( "IV" );
+                    break;
+                case 5:
+                    System.out.println( "V" );
+                    break;
+                case 6:
+                    System.out.println( "VI" );
+                    break;
+                case 7:
+                    System.out.println( "VII" );
+                    break;
+                case 8:
+                    System.out.println( "VIII" );
+                    break;
+                case 9:
+                    System.out.println( "IX" );
+                    break;
+                case 10:
+                    System.out.println( "X" );
+                    break;
+            }
+        }
+
     }
 
     void multiplication() {
         int resultatMultiplication = number1 * number2; // вычисление умножения
         System.out.println( "Output" );
-        System.out.println( +resultatMultiplication );
+        if ( s1 == 1 ) {
+            System.out.println( +resultatMultiplication );
+        } else {
+            switch ( resultatMultiplication ) {
+                case 1:
+                    System.out.println( "I" );
+                    break;
+                case 2:
+                    System.out.println( "II" );
+                    break;
+                case 3:
+                    System.out.println( "III" );
+                    break;
+                case 4:
+                    System.out.println( "IV" );
+                    break;
+                case 5:
+                    System.out.println( "V" );
+                    break;
+                case 6:
+                    System.out.println( "VI" );
+                    break;
+                case 7:
+                    System.out.println( "VII" );
+                    break;
+                case 8:
+                    System.out.println( "VIII" );
+                    break;
+                case 9:
+                    System.out.println( "IX" );
+                    break;
+                case 10:
+                    System.out.println( "X" );
+                    break;
+            }
+        }
+
     }
 
     void division() {
         int resultatDivision = number1 / number2; // вычисление деления
         System.out.println( "Output" );
-        System.out.println( +resultatDivision );
+        if ( s1 == 1 ) {
+            System.out.println( +resultatDivision );
+        } else {
+            switch ( resultatDivision ) {
+                case 1:
+                    System.out.println( "I" );
+                    break;
+                case 2:
+                    System.out.println( "II" );
+                    break;
+                case 3:
+                    System.out.println( "III" );
+                    break;
+                case 4:
+                    System.out.println( "IV" );
+                    break;
+                case 5:
+                    System.out.println( "V" );
+                    break;
+                case 6:
+                    System.out.println( "VI" );
+                    break;
+                case 7:
+                    System.out.println( "VII" );
+                    break;
+                case 8:
+                    System.out.println( "VIII" );
+                    break;
+                case 9:
+                    System.out.println( "IX" );
+                    break;
+                case 10:
+                    System.out.println( "X" );
+                    break;
+            }
+        }
+
     }
 }
